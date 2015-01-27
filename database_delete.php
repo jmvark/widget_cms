3fdf26adda88c033f0fdb23773edc1a99f350494
JasonMax_2 <jason-max.vark@khk.ee>
2015-01-27 13:06:51 +0200

    database_del commit

---
+++ database_delete.php
@@ -0,0 +1,36 @@
+<?php if (!isset($_GET['id']) or $_GET != 0) {
+    header('location: index.php');
+  }
+  require('Components/config.php');
+	$id = $_GET['id'];
+	$query = "DELETE FROM subjects where id= {$id}";
+	$result = mysqli_query($connection, $query);
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2; url=index.php/">
+    <title>Document</title>
+    <style>
+	    .notice {
+	    	color:green;
+	    }
+	    .error {
+	    	color: red;
+	    }
+    </style>
+  </head>
+
+  <body>
+  	<?php if ($result && mysqli_affected_rows($connection)) { ?>
+	  <p class="notice"><?php echo "Rida kustutatud"; ?></p>
+	<?php } else { ?>
+	  <p class="error"><?php echo "Sellist rida andmebaasis ei ole"; ?></p>
+	<?php } ?>
+
+  </body>
+</html>
+
+<?php mysqli_close($connection);?>
